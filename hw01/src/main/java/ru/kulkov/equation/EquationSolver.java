d181a29b707cc817704719292a41d92cc4354565
Yaroslav Kulkov <y_mail@mail.ru>
2025-07-21 14:15:00 +0300

    Added realization for x^2+2x+1 = 0

--- hw01/src/main/java/ru/kulkov/equation/EquationSolver.java
+++ hw01/src/main/java/ru/kulkov/equation/EquationSolver.java
@@ -1,12 +1,19 @@
 package ru.kulkov.equation;
 
 public class EquationSolver implements Solver{
+
+    private static final double EPSILON = 1e-10;
+
     @Override
     public double[] solve(double a, double b, double c) {
         double d = findDiscriminant(a, b, c);
 
         if (d < 0) {
             return new double[0];
+        }
+        if (Math.abs(d) <= EPSILON) {
+            double root = -b / (2 * a);
+            return new double[]{root, root};
         }
 
         double root1 = (-b + Math.sqrt(d)) / 2 * a;
