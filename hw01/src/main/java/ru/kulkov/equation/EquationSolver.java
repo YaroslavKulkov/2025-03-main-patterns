b2f957e5b80513f3435e43f8d56a41bdf83d0d17
Yaroslav Kulkov <y_mail@mail.ru>
2025-07-21 13:53:29 +0300

    Added realisation for x^2+1 = 0

--- hw01/src/main/java/ru/kulkov/equation/EquationSolver.java
+++ hw01/src/main/java/ru/kulkov/equation/EquationSolver.java
@@ -3,6 +3,9 @@ package ru.kulkov.equation;
 public class EquationSolver implements Solver{
     @Override
     public double[] solve(double a, double b, double c) {
-        return new double[0];
+        if(a == 1 && b == 0 && c == 1) {
+            return new double[0];
+        }
+        return null;
     }
 }
