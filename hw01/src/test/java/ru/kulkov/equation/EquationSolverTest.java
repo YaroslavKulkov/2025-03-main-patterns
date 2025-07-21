4e1cd343fa8c096d598c38444ed019db6d271af9
Yaroslav Kulkov <y_mail@mail.ru>
2025-07-21 13:57:05 +0300

    Added test for x^2-1 = 0

--- hw01/src/test/java/ru/kulkov/equation/EquationSolverTest.java
+++ hw01/src/test/java/ru/kulkov/equation/EquationSolverTest.java
@@ -13,9 +13,18 @@ public class EquationSolverTest {
     }
 
     @Test
-    void solve_WhenAZeroBZeroCOne_ReturnsEmptyArray() {
+    void solve_WhenAOneBZeroCOne_ReturnsEmptyArray() {
+        //Написать тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
         double[] actual = solver.solve(1, 0,1);
         double[] expected = new double[0];
         assertArrayEquals(expected, actual, "Должен вернуться пустой массив");
+    }
+
+    @Test
+    void solve_WhenAOneBZeroCMinusOne_ReturnsRoots(){
+        //Написать тест, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)
+        double[] actual = solver.solve(1, 0,-1);
+        double[] expected = new double[]{1, -1};
+        assertArrayEquals(expected, actual, "Должны быть получены корни (x1=1, x2=-1)");
     }
 }
